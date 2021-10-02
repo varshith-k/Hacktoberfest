81d1d3a3120591d9bae6b6a58a0cf468188d312f
Aryan810 <67587135+Aryan810@users.noreply.github.com>
2021-10-02 07:13:55 +0530

    Add files via upload

---
+++ new_serach_algorithm.py
@@ -0,0 +1,50 @@
+from threading import Thread
+import time
+
+
+class SearchAlgorithm1:
+
+    def __init__(self, list_data: list, element):
+        self.element = element
+        self.list = list_data
+        self.searching = True
+        self.index = None
+
+    def search(self):
+        Thread(self.forward())
+        self.reverse()
+        return self.index
+
+    def reverse(self):
+        len_all = len(self.list)
+        for i in range(len(self.list) - 1):
+            try:
+                if not self.searching:
+                    break
+                i = len_all - i
+                if self.list[i] == self.element:
+                    self.searching = False
+                    self.index = i
+                    break
+            except Exception:
+                pass
+
+    def forward(self):
+        for i in range(len(self.list) - 1):
+            try:
+                if not self.searching:
+                    break
+                if self.list[i] == self.element:
+                    self.searching = False
+                    self.index = i
+                    break
+            except Exception:
+                pass
+
+
+list_i = [i for i in range(1, 100)]
+print("Searching by my algorithm...")
+
+start = time.sleep()
+index_element = SearchAlgorithm1(list_i, 2).search()
+print(index_element)
