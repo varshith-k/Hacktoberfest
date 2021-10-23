532ddbab8ed59d263f38d7d9b674b6f986b0ab34
durgajaswanth <73519404+durgajaswanth@users.noreply.github.com>
2021-10-24 00:58:47 +0530

    My First C program

---
+++ Age.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define pi 3.14
+int main(void)
+{ int x,y,d;
+printf("Enter Age");
+scanf("%d",&x);
+int x;
+int x;
+y = x+10;
+printf("You are aged %d years old in 2029\n",y);
+return 0;
+}
